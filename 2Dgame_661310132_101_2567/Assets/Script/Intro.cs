7b50b9c2ad6a24df40d1a15a54931e94f8029d9c
Prem661310132 <jongjityotin_p@silpakorn.edu>
2025-01-24 22:22:26 +0700

    there is intro I think

---
+++ 2Dgame_661310132_101_2567/Assets/Script/Intro.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+ 
+    public float wait_time = 5f;
+    void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait_for_intro()
+    { 
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(1);
+    }
+}
